63559c7bda42664c7e28a7e6239d53983d1e2efd
Ariel <ari2006ladner@gmail.com>
2026-02-12 15:09:49 -0300

    feat: Add Mini Projects section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,7 +17,7 @@ export const projects: Project[] = [
         title: "WonderLabs Web",
         shortDescription: "Sitio web para agencia de desarrollo web.",
         description: "Desarrollé una web moderna para una agencia de desarrollo web utilizando Astro y Tailwind.",
-        image: "/images/projects/wonder.png",
+        image: "https://lh3.googleusercontent.com/aida-public/AB6AXuA_KKf2e7jkxljN9iv8lgXD6KAIn-xBksC6Xw6wVbqFbcVj5ofwsP1ljONLzYbfUOzIrcT6RPy4oYmu2Gnb6S-39WoMz9PaCeYnOOTbxi7CTGGDsf9FaQI9yoMtzfNtN0IrtlmM2QNrGY8p8-7iU2cdybwgkPui-xwf-C0H1BNcrht0WMhY8xkp6ZY8FFP_3wgQprwXLECMGlYFBdc6x2XxvwaYxPcpJLLHRG7eMrcXc8pISy_y6nrhDm4BekxprfG6bb1OvP6b2Z4",
         tech: ["Astro", "Tailwind"],
         repo: "",
         demo: "",
@@ -29,11 +29,23 @@ export const projects: Project[] = [
         title: "LinkHub - NicoDev",
         shortDescription: "Sitio web para un creador de contenido.",
         description: "Desarrollé una web moderna para un creador de contenido utilizando Astro y Tailwind.",
-        image: "/images/projects/linkhub.png",
+        image: "https://lh3.googleusercontent.com/aida-public/AB6AXuBDsGiHsant14g0EVuIp508Isqgf60WDKvf7tvsEryaQ8_cG1mhh0zHA11d2xHWH3FAe66uGVLsbWujmAieCHLjPlaX29I8IUJ6FH6Af5Pu1sILC0qyEuNVUJxwJIbVXyrG5TBlKxK4fmEZ1HXSFOZcnpfVeKtHUsr6BuQBc6Q0wpuaS-H3Xh_SJfzm8sZR8v2hiJykX2xGD3OV9JGZK0pfNLGyiBtzb0aGm6jZHZ9DQ-clffZcuA2ili2Mnd52r-wcfd7qZbTqwas",
         tech: ["Astro", "Tailwind"],
         repo: "",
         demo: "",
         featured: true,
         year: 2026
+    },
+    {
+        slug: "wonder-web",
+        title: "WonderLabs Web",
+        shortDescription: "Sitio web para agencia de desarrollo web.",
+        description: "Desarrollé una web moderna para una agencia de desarrollo web utilizando Astro y Tailwind.",
+        image: "https://lh3.googleusercontent.com/aida-public/AB6AXuA_KKf2e7jkxljN9iv8lgXD6KAIn-xBksC6Xw6wVbqFbcVj5ofwsP1ljONLzYbfUOzIrcT6RPy4oYmu2Gnb6S-39WoMz9PaCeYnOOTbxi7CTGGDsf9FaQI9yoMtzfNtN0IrtlmM2QNrGY8p8-7iU2cdybwgkPui-xwf-C0H1BNcrht0WMhY8xkp6ZY8FFP_3wgQprwXLECMGlYFBdc6x2XxvwaYxPcpJLLHRG7eMrcXc8pISy_y6nrhDm4BekxprfG6bb1OvP6b2Z4",
+        tech: ["Astro", "Tailwind"],
+        repo: "",
+        demo: "",
+        featured: true,
+        year: 2025
     }
 ];
